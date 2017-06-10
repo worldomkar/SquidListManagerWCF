5af29c9b8f40390b06b3b454c504d7035bacc9a9
Omkar P <omkar.patekar@gmail.com>
2017-06-10 19:09:26 +1000

    Update Form name and search functionality
    Added search (F3), ignore-all functionality.

---
+++ Squid Monitor/Squid Monitor/SquidMonitor.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Squid_Monitor
+{
+    partial class SquidMonitor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.reloadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reloadNewDomainsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ignoreAllNewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtSearchBox = new System.Windows.Forms.TextBox();
+            this.btnClearSearch = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 27);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(491, 666);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.ItemDrag += new System.Windows.Forms.ItemDragEventHandler(this.treeView1_ItemDrag);
+            this.treeView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.treeView1_DragDrop);
+            this.treeView1.DragEnter += new System.Windows.Forms.DragEventHandler(this.treeView1_DragEnter);
+            this.treeView1.DragOver += new System.Windows.Forms.DragEventHandler(this.treeView1_DragOver);
+            this.treeView1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.treeView1_KeyUp);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.reloadToolStripMenuItem,
+            this.reloadNewDomainsToolStripMenuItem,
+            this.ignoreAllNewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(515, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // reloadToolStripMenuItem
+            // 
+            this.reloadToolStripMenuItem.Name = "reloadToolStripMenuItem";
+            this.reloadToolStripMenuItem.Size = new System.Drawing.Size(88, 20);
+            this.reloadToolStripMenuItem.Text = "Reload Squid";
+            this.reloadToolStripMenuItem.ToolTipText = "Ask Squid service to reload trust and block list files and re-configure.";
+            this.reloadToolStripMenuItem.Click += new System.EventHandler(this.reloadToolStripMenuItem_Click);
+            // 
+            // reloadNewDomainsToolStripMenuItem
+            // 
+            this.reloadNewDomainsToolStripMenuItem.Name = "reloadNewDomainsToolStripMenuItem";
+            this.reloadNewDomainsToolStripMenuItem.Size = new System.Drawing.Size(141, 20);
+            this.reloadNewDomainsToolStripMenuItem.Text = "Re-query NewDomains";
+            this.reloadNewDomainsToolStripMenuItem.ToolTipText = "Get a list of un-categorized domains from SquidManager WCF service.";
+            this.reloadNewDomainsToolStripMenuItem.Click += new System.EventHandler(this.reloadNewDomainsToolStripMenuItem_Click);
+            // 
+            // ignoreAllNewToolStripMenuItem
+            // 
+            this.ignoreAllNewToolStripMenuItem.Name = "ignoreAllNewToolStripMenuItem";
+            this.ignoreAllNewToolStripMenuItem.Size = new System.Drawing.Size(95, 20);
+            this.ignoreAllNewToolStripMenuItem.Text = "Ignore all New";
+            this.ignoreAllNewToolStripMenuItem.ToolTipText = "Move all new domains to ignore list (Blocked list->Global->Inactive)";
+            this.ignoreAllNewToolStripMenuItem.Click += new System.EventHandler(this.ignoreAllNewToolStripMenuItem_Click);
+            // 
+            // txtSearchBox
+            // 
+            this.txtSearchBox.Location = new System.Drawing.Point(336, 4);
+            this.txtSearchBox.Name = "txtSearchBox";
+            this.txtSearchBox.Size = new System.Drawing.Size(141, 20);
+            this.txtSearchBox.TabIndex = 2;
+            this.txtSearchBox.TextChanged += new System.EventHandler(this.txtSearchBox_TextChanged);
+            this.txtSearchBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSearchBox_KeyPress);
+            // 
+            // btnClearSearch
+            // 
+            this.btnClearSearch.Location = new System.Drawing.Point(483, 4);
+            this.btnClearSearch.Name = "btnClearSearch";
+            this.btnClearSearch.Size = new System.Drawing.Size(32, 20);
+            this.btnClearSearch.TabIndex = 3;
+            this.btnClearSearch.Text = "X";
+            this.btnClearSearch.UseVisualStyleBackColor = true;
+            this.btnClearSearch.Click += new System.EventHandler(this.btnClearSearch_Click);
+            // 
+            // SquidMonitor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 705);
+            this.Controls.Add(this.btnClearSearch);
+            this.Controls.Add(this.txtSearchBox);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "SquidMonitor";
+            this.Text = "Squid Manager - Live";
+            this.Activated += new System.EventHandler(this.SquidMonitor_Activated);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem reloadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reloadNewDomainsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ignoreAllNewToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtSearchBox;
+        private System.Windows.Forms.Button btnClearSearch;
+    }
+}
+
---
+++ Squid Monitor/Squid Monitor/SquidMonitor.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Squid_Monitor.SquidManager;
+using System.Diagnostics;
+
+namespace Squid_Monitor
+{
+    public partial class SquidMonitor : Form
+    {
+        SquidManager.SqMgrClient sm = new SquidManager.SqMgrClient();//("ISqMgr", "http://localhost:8733/Design_Time_Addresses/sqmgr_wcf_svc/Service1/");
+        public SquidMonitor()
+        {
+            InitializeComponent();
+        }
+
+        public delegate int Add(TreeNode tn);
+
+        private TreeNode ConstructTreeFromDlist(SquidManager.DList dl)
+        {
+            if (null != dl)
+            {
+                TreeNode tnNewRoot = new TreeNode();
+                tnNewRoot.Expand();
+                if (0 == dl.Sections.Length)
+                {
+                    dl.Sections = new Section[1];
+                    Section s = dl.Sections[0] = new Section();
+                    s.Name = "Global";
+                    s.ActiveDomain = new string[0];
+                    s.InactiveDomain = new string[0];
+                }
+                foreach (SquidManager.Section s in dl.Sections)
+                {
+                    TreeNode tnSection = new TreeNode(s.Name);
+                    TreeNode tnActive = new TreeNode("Active");
+                    TreeNode tnInactive = new TreeNode("Inactive");
+                    foreach (string domain in s.ActiveDomain)
+                    {
+                        TreeNode dmn = new TreeNode(domain);
+                        tnActive.Nodes.Add(dmn);
+                        tn.Add(dmn);
+                    }
+                    foreach (string domain in s.InactiveDomain)
+                    {
+                        TreeNode dmn = new TreeNode(domain);
+                        tnInactive.Nodes.Add(dmn);
+                        tn.Add(dmn);
+                    }
+                    tnSection.Nodes.Add(tnActive);
+                    tnSection.Nodes.Add(tnInactive);
+                    tnNewRoot.Nodes.Add(tnSection);
+                }
+                return tnNewRoot;
+            }
+            return null;
+        }
+
+        TreeNode tnNewDomains = null;
+        void LoadList(string listName, string listType)
+        {
+            SquidManager.DList dl = null;
+            switch (listType.ToLower())
+            {
+                case "trust":
+                    dl = sm.GetTrustList();
+                    break;
+                case "block":
+                    dl = sm.GetBlockList();
+                    break;
+                case "new":
+                    dl = sm.GetNewDomains();
+                    break;
+            }
+            TreeNode tnNewRoot = ConstructTreeFromDlist(dl);
+            switch (listType.ToLower())
+            {
+                case "trust":
+                    tnNewRoot.Nodes[19].Expand();
+                    break;
+                case "block":
+                    tnNewRoot.Nodes[0].Expand();
+                    BlockIgnore = tnNewRoot.Nodes[0].Nodes[1];
+                    break;
+                case "new":
+                    if (0 < tnNewRoot.Nodes.Count)
+                    {
+                        tnNewRoot.Nodes[0].Expand();
+                        tnNewRoot.Nodes[0].Nodes[1].Expand();
+                    }
+                    tnNewDomains = tnNewRoot;
+                    break;
+            }
+            tnNewRoot.Text = listName;
+            treeView1.Invoke(new Add(treeView1.Nodes.Add), tnNewRoot);
+        }
+
+        void LoadData()
+        {
+            LoadList("Trusted Domains", "trust");
+            LoadList("Blocked Domains", "block");
+            LoadList("New Domains", "new");
+            loadCompleted = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread loader = new Thread(LoadData);
+            loader.Start();
+            treeView1.AllowDrop = true;
+        }
+
+        private void treeView1_ItemDrag(object sender, ItemDragEventArgs e)
+        {
+            Console.WriteLine(e.Item.ToString());
+            if (((TreeNode)e.Item).Text[0] == '.')
+            {
+                DoDragDrop(e.Item, DragDropEffects.Move | DragDropEffects.Scroll);
+            }
+        }
+
+        static int i = 0;
+        private void treeView1_DragDrop(object sender, DragEventArgs e)
+        {
+            Console.WriteLine("In DragDrop");
+            if (e.Data.GetDataPresent("System.Windows.Forms.TreeNode", false))
+            {
+                Point pt = ((TreeView)sender).PointToClient(new Point(e.X, e.Y));
+                TreeNode Section = ((TreeView)sender).GetNodeAt(pt);
+                TreeNode Domain = (TreeNode)e.Data.GetData("System.Windows.Forms.TreeNode");
+                if ((null != Section) &&
+                    (Section != Domain) &&
+                    (null != Section.Parent) &&
+                    (null != Section.Parent.Parent))
+                {
+                    switch (Section.Parent.Parent.Text.ToLower())
+                    {
+                        case "trusted domains":
+                            sm.AddNewDomain(Domain.Text, "trust", Section.Parent.Text, Section.Text);
+                            break;
+                        case "blocked domains":
+                            sm.AddNewDomain(Domain.Text, "block", Section.Parent.Text, Section.Text);
+                            break;
+                    }
+                    Domain.Parent.Nodes.Remove(Domain);
+                    Section.Nodes.Add(Domain);
+                }
+            }
+        }
+
+        private void treeView1_DragEnter(object sender, DragEventArgs e)
+        {
+            Console.WriteLine("In DragEnter");
+            e.Effect = DragDropEffects.Move | DragDropEffects.Scroll;
+        }
+
+        private void treeView1_DragOver(object sender, DragEventArgs e)
+        {
+            i++;
+            Console.WriteLine("In DragOver" + i);
+            if (e.Data.GetDataPresent("System.Windows.Forms.TreeNode", false))
+            {
+                Point pt = ((TreeView)sender).PointToClient(new Point(e.X, e.Y));
+                TreeNode Section = ((TreeView)sender).GetNodeAt(pt);
+                TreeNode Domain = (TreeNode)e.Data.GetData("System.Windows.Forms.TreeNode");
+                if ((null != Section) &&
+                    (Section != Domain) &&
+                    (null != Section.Parent) &&
+                    (null != Section.Parent.Parent))
+                {
+                    e.Effect = DragDropEffects.Move | DragDropEffects.Scroll;
+                }
+                else
+                {
+                    e.Effect = DragDropEffects.None;
+                }
+            }
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            treeView1.Height = SquidMonitor.ActiveForm.Height - 80;
+        }
+
+        private void reloadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            sm.ReloadLists();
+        }
+
+        private void reloadNewDomains ()
+        {
+            try
+            {
+                SquidManager.DList dl = sm.GetNewDomains();
+                TreeNode tnNewDomains = ConstructTreeFromDlist(dl);
+                tnNewDomains.Text = "New Domains";
+                tnNewDomains.Nodes[0].Expand();
+                tnNewDomains.Nodes[0].Nodes[1].Expand();
+                foreach (TreeNode n in tnNewDomains.Nodes[0].Nodes[1].Nodes)    // Add new nodes
+                {
+                    TreeNode[] tnCol = treeView1.Nodes[2].Nodes[0].Nodes[1].Nodes.Cast<TreeNode>()
+                        .Where(r => (r.Text == n.Text))
+                        .ToArray();
+                    if (0 == tnCol.Count())
+                    {
+                        treeView1.Nodes[2].Nodes[0].Nodes[1].Nodes.Add(n);
+                        tn.Add(n);
+                    }
+                }
+            } catch (Exception e) { }
+        }
+
+        private void reloadNewDomainsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            reloadNewDomains();
+        }
+
+        private void ignoreAllNewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if ((null != tnNewDomains) && (null != tnNewDomains.Nodes[0]) &&
+                (null != tnNewDomains.Nodes[0].Nodes[1]) && (null != tnNewDomains.Nodes[0].Nodes[1].Nodes))
+            {
+                int iNodes = tnNewDomains.Nodes[0].Nodes[1].Nodes.Count;
+                for (int i = 0; i < iNodes; ++i)
+                {
+                    TreeNode n = tnNewDomains.Nodes[0].Nodes[1].Nodes[i];
+                    sm.AddNewDomain (n.Text, "block", "Global", "inactive");
+                }
+                for (int i = 0; i < iNodes; ++i)
+                {
+                    TreeNode n = tnNewDomains.Nodes[0].Nodes[1].Nodes[0];
+                    n.Parent.Nodes.Remove(n);
+                    BlockIgnore.Nodes.Add(n);
+                }
+            }
+        }
+
+        private void btnClearSearch_Click(object sender, EventArgs e)
+        {
+            txtSearchBox.Clear();
+            focusedNode = 0;
+            txtSearchChanged = true;
+            foreach (TreeNode List in treeView1.Nodes)
+                foreach (TreeNode Section in List.Nodes)
+                {
+                    foreach (TreeNode subList in Section.Nodes)
+                        subList.Collapse();
+                    if ((0 != String.Compare(Section.Text, "Miscelleneous", StringComparison.OrdinalIgnoreCase)) &&
+                        ((0 != String.Compare(Section.Text, "Global", StringComparison.OrdinalIgnoreCase)) ||
+                        (Section.Parent.Text.Contains("Trusted Domains"))))
+                    {
+                        Section.Collapse();
+                    }
+                }
+        }
+
+        private TreeNode BlockIgnore = null;
+        private bool loadCompleted = false;
+        private void SquidMonitor_Activated(object sender, EventArgs e)
+        {
+            if (!loadCompleted)
+                return;
+            reloadNewDomains();
+        }
+
+        bool txtSearchChanged = false;
+        private void txtSearchBox_TextChanged(object sender, EventArgs e)
+        {
+            txtSearchChanged = true;
+        }
+
+        List<TreeNode> tn = new List<TreeNode>();
+        List<TreeNode> searchList = new List<TreeNode>();
+        int focusedNode = 0;
+        private void txtSearchBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                selectNextSearchedNode();
+            }
+        }
+
+        private void selectNextSearchedNode()
+        {
+            if (txtSearchChanged || (0 == searchList.Count))
+            {
+                searchList = tn.Where(x => (x.Text.Contains(txtSearchBox.Text))).ToList();
+                focusedNode = 0;
+                txtSearchChanged = false;
+            }
+            try
+            {
+                treeView1.Select();
+                treeView1.SelectedNode = searchList[focusedNode];
+            }
+            catch (Exception e2)
+            {
+
+            }
+            ++focusedNode;
+            if (focusedNode >= searchList.Count)
+                focusedNode = 0;
+        }
+
+        private void treeView1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F3)
+            {
+                if (e.Shift)
+                {
+                    focusedNode -= 2;
+                    if (focusedNode < 0)
+                        focusedNode += searchList.Count;
+                    if (focusedNode < 0)
+                        focusedNode = 0;
+                }
+                selectNextSearchedNode();
+            }
+        }
+    }
+}
